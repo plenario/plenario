9448d9383cc4f54ebb5a30c94aeba321b1fbb1b7
Ana Schwendler <schwendler@gmail.com>
2017-08-18 15:01:53 +0300

    First app commit

---
+++ README.md
@@ -0,0 +1,8 @@
+clone the project
+
+setup for flask
+
+to fully run the project:
+```
+python run.py
+```
---
+++ app/views.py
@@ -0,0 +1,30 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+  return render_template("index.html", title='Home')
+
+@app.route('/senators')
+def senators():
+    user = {'nickname': 'Ana'}  # fake user
+    senators = [  # array of senators
+        {
+            'senator': {'nickname': 'Acir Gurgacz'},
+            'party': 'PDT-RO'
+        },
+        {
+            'senator': {'nickname': 'Aécio Neves'},
+            'party': 'PSDB-MG'
+        }
+    ]
+    return render_template("senators.html", title='Senadores', senators=senators)
+
+@app.route('/propositions')
+def propositions():
+  return render_template("propositions.html", title='Proposições')
+
+@app.route('/about')
+def about():
+  return render_template("about.html", title='Sobre')
